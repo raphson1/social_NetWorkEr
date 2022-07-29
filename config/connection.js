0fbb5ca0d740a885f0d59d61664b2a1bc826f3ea
raphson1 <raphsoneric@gmail.com>
2022-07-28 19:58:18 -0500

    connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString = 
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myApiDB';
+
+    connect(connectionString, {
+        useNewUrlParser: true,
+        useUniFiedTopology: true
+    });
+
+    module.exports = connection;
