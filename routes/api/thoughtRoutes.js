3bf828592de843b72665a0e3d1f33869dc9162f4
raphson1 <raphsoneric@gmail.com>
2022-08-10 11:27:22 -0500

    thought and reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,3 +11,12 @@ const {
 
 } = require('../../controllers/thoughtController');
 
+router.route('/').get(getThought).post(createThougnt);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route(':thoughtId/reactions').post(createReaction);
+
+router.route('/thoughtzid/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
