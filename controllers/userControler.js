de9792cecbfb704e8f858f4826f8d38e8ff84152
raphson1 <raphsoneric@gmail.com>
2022-08-13 12:44:16 -0500

    add friend and update

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -39,8 +39,31 @@ module.exports = {
                  : res.json(user)
             )
             .catch((err) => res.status(500).json(err))
-    }
+    },
 
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) => 
+            !user
+              ? res.status(404).json({ message: 'No User Find with this ID!' })
+              : Thought .deleteMany({ _id: { $in: user.thoughts } })
+          )
+          .then(() => res.json({ message: 'User and Thought deleted!' }))
+          .catch((err) => res.status(500).json(err))
+    },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) => 
+              !user
+                ? res.status(404).json({ message: 'No User find with this Id!' })
+                : res.json(user)
+            )
+    }
     
     
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,16 +4,16 @@ const {
     getUser,
     getSingleUser,
     createUser,
-    // updateUser,
-    // deleteUser,
-    // addFriend,
-    // deleteFriend
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/userControler');
 
 router.route('/').get(getUser).post(createUser);
 
 router.route('/:userId').get(getSingleUser);
-// .put(updateUser).delete(deleteUser);
+.put(updateUser).delete(deleteUser);
 
 
 // router.route('/:userId/friends/:friendId')
