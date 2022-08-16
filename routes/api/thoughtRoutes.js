e3946c3cf663e9ea027a0af03e276080bb84652b
raphson1 <raphsoneric@gmail.com>
2022-08-15 22:33:54 -0500

    APIs debug

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const {
     getThought,
     getSingleThought,
-    createThougnt,
+    createThought,
     updateThought,
     deleteThought,
     createReaction,
@@ -11,13 +11,12 @@ const {
 
 } = require('../../controllers/thoughtController');
 
-// router.route('/').get(getThought).post(createThougnt);
+router.route('/').get(getThought).post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought);
-// router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-// router.route(':thoughtId/reactions').post(createReaction);
+router.route(':thoughtId/reactions').post(createReaction);
 
-// router.route('/thoughtsid/reactions/:reactionId').delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,11 +12,9 @@ const {
 
 router.route('/').get(getUser).post(createUser);
 
-router.route('/:userId').get(getSingleUser);
-// .put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 
-// router.route('/:userId/friends/:friendId')
-// .post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
